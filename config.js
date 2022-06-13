7c6ee06207cf2c9d1595cf92efa0650ed8086f80
elaina-mwol <106641376+elaina-mwol@users.noreply.github.com>
2022-06-13 21:30:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -39,8 +39,8 @@ global.APIKeys = {
 // Other
 global.owner = ['27634090203','447441437150']
 global.premium = ['27634090203','447441437150']
-global.botname = 'ZIM BOT INC'
-global.packname = 'ZIM BOT INC'
+global.botname = 'Vihanga - MD🥀'
+global.packname = 'Vihanga - MD🥀'
 global.monayawal = '100000'
 global.darahawal = '100'
 global.besiAwal = '15'
@@ -55,7 +55,7 @@ global.f3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.docu
 global.f4 = 'application/zip'
 global.f5 = 'application/pdf'
 global.f6 = 'application/vnd.android.package-archive'
-global.author = 'ZIM BOT INC'
+global.author = 'Vihanga - MD🥀'
 global.flaming = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text='
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
@@ -69,7 +69,7 @@ global.mess = {
     group: '*this feature is only for groups okay*',
     private: '*this feature can only used in dm okay*',
     bot: '*Special Features for User Bot Number okay*',
-    wait: '*whoa wait zim bot processing okay*',
+    wait: '*ටිකක් ඉන්න 🍃...*',
     endLimit: '*Brothers Daily Limit Has Expired, Type .buy limit amount limit to buy limit, Note: Limit will be reset every 12 hours*',
 }
 global.limitawal = {
